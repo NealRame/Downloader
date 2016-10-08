f819eff8b47399df713edb772f32978af451581b
NealRame <contact@nealrame.com>
2016-10-09 01:13:26 +0200

    add command line args parser

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
+const pkg = require('./package.json');
 const path = require('path');
+const optimist = require('optimist');
 
 const common = require('./common');
 const core = require('./core');
 const ui = require('./ui');
 
-const base_url = process.argv[2];
+const argv = optimist
+	.usage(`Usage: ${pkg.name} [OPTIONS] REMOTE_DIR_URL`)
+	.wrap(80)
+	.options('h', {
+		alias: 'help',
+		boolean: true,
+		describe: 'print this help and return.'
+	})
+	.options('o', {
+		alias: 'output-dir',
+		default: process.cwd(),
+		describe: 'set output directory.'
+	})
+	.options('p', {
+		alias: 'show-progress',
+		boolean: true,
+		describe: 'display a progress bar.'
+	})
+	.options('V', {
+		alias: 'version',
+		boolean: true,
+		describe: 'print the version and return.'
+	})
+	.argv;
 
-core.fetchRemoteDir(base_url)
+function get_progress_bar_builder() {
+	if (argv['show-progress']) {
+		return ui.makeProgressBar;
+	}
+}
+
+if (argv.help) {
+	optimist.showHelp();
+	process.exit(0);
+}
+
+if (argv.version) {
+	console.log(pkg.version);
+	process.exit(0);
+}
+
+const [base_url] = argv._;
+const output_dir = argv['output-dir'];
+
+common.mkdir(output_dir)
+	.then(() => core.fetchRemoteDir(base_url))
 	.then((links) => {
 		return common.eachSeries(
 			links,
 			(link) => {
 				const remote_file_url = `${base_url}/${link}`;
-				const output_file = path.basename(link);
+				const output_file = path.join(output_dir, path.basename(link));
 				console.log(`-- downloading ${link}`);
-				return core.download(remote_file_url, output_file, ui.makeProgressBar);
+				return core.download(remote_file_url, output_file, get_progress_bar_builder());
 			}
 		);
 	})

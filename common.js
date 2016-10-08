a0fc936bd977dd6b9de9b6a1d9e91d0720495ebf
NealRame <contact@nealrame.com>
2016-10-09 01:12:32 +0200

    add mkdir to common functions

--- common.js
+++ common.js
@@ -1,4 +1,6 @@
 const each = require('async/eachSeries');
+const fs = require('fs');
+const fsextra = require('fs.extra');
 const is_nil = require('lodash.isnil');
 
 function nodify(resolve, reject) {
@@ -11,6 +13,12 @@ function nodify(resolve, reject) {
 	};
 }
 
+function make_promise(fn, ...args) {
+	return new Promise((resolve, reject) => {
+		fn(...args, nodify(resolve, reject));
+	})
+}
+
 function each_series(collection, task) {
 	return new Promise((resolve, reject) => {
 		each(
@@ -21,6 +29,23 @@ function each_series(collection, task) {
 	});
 }
 
+function mkdir(dir_path) {
+	return make_promise(fs.stat, dir_path)
+		.then((stats) => {
+			if (stats.isDirectory()) {
+				return Promise.resolve();
+			}
+			throw new Error(`${dir_path} exists but is not a directory!`);
+		})
+		.catch((err) => {
+			if (err.code === 'ENOENT') {
+				return make_promise(fsextra.mkdirp, dir_path);
+			}
+			throw err;
+		});
+}
+
 module.exports = {
-	eachSeries: each_series
+	eachSeries: each_series,
+	mkdir
 };
